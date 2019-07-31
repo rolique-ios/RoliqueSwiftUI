efd165cc41c9a29fd6c3bfcae6cd470d8bffa655
bbb <bbbsavych@gmail.com>
2019-07-31 12:29:13 +0300

    Initial commit
    Added .gitignore,
    added Sourcery with equtable, hashable templates

---
+++ ROLIQUE/ROLIQUE/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ROLIQUE
+//
+//  Created by Bohdan Savych on 7/31/19.
+//  Copyright © 2019 Bohdan Savych. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
