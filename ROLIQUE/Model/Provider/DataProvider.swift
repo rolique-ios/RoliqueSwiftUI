c7fd74d9630351e213350484b770e3786e3e4260
Andrii Narinian <andrii.narinian@rolique.io>
2019-08-02 13:07:39 +0300

    Introduce Action

--- ROLIQUE/Model/Provider/DataProvider.swift
+++ ROLIQUE/Model/Provider/DataProvider.swift
@@ -10,9 +10,6 @@ import Foundation
 import Networking
 import Combine
 
-public struct Action {
-   public class Late {}
-}
 
 public struct Fetch<M: ModelType> {}
 
@@ -20,53 +17,16 @@ public class FetchArray<A: ModelType> {}
 
 public class FetchUsers: FetchArray<User> {}
 
-//TODO: implement combine Publisher
-public extension Action.Late {
-  struct Demand {
-    public let from: String
-    public let value: String
-    public let sender: String
-    
-    public init (from: String,value: String,sender: String) {
-      self.from = from
-      self.value = value
-      self.sender = sender
-    }
-  }
+protocol ProviderProtocol {
+  associatedtype ModelType
+  //@
+  var bindableObject: ModelType? { get }
   
-  static func please(demand: Demand, publisher: PassthroughSubject<ActionResult, Error>) {
-    Net.Worker.request(
-      Late(sender: demand.sender, from: demand.from, value: demand.value, isTest: true),
-      onSuccess: {
-        let result = ActionResult(map: Json.parse($0.stringValue))
-        publisher.send(result)
-    },
-      onError: {
-        print($0)
-        publisher.send(ActionResult(error: $0))
-    })
-  }
-  
-  static func fromNow(value: String, sender: String, result: ((ActionResult) -> Void)?) {
-    Net.Worker.request(
-      Late(sender: sender, from: "now", value: value, isTest: true),
-      onSuccess: {
-        result?(ActionResult(map: Json.parse($0.stringValue)))
-    },
-      onError: {
-        result?(ActionResult(error: $0))
-    })
-  }
-  
-  static func fromStart(value: String, sender: String, result: ((ActionResult) -> Void)?) {
-    Net.Worker.request(
-      Late(sender: sender, from: "start", value: value, isTest: true),
-      onSuccess: {
-        result?(ActionResult(map: Json.parse($0.stringValue)))
-    },
-      onError: {
-        result?(ActionResult(error: $0))
-    })
-  }
 }
 
+class Provider<M: Model>: ProviderProtocol {
+  var bindableObject: M?
+  
+  typealias ModelType = M
+  
+}
